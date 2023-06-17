dea6fd0d1e3ffdfd172bcce7f4d055a2c98ce859
makwalam <makwala7@gmail.com>
2023-06-17 12:09:55 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * Retuen : 0 (seccuss)
+ */
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+
+    return (0);
+}
