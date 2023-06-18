89f21081328af1767a58507b352c5f8a0ca70c83
makwalam <makwala7@gmail.com>
2023-06-18 07:10:03 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++;
+	}
+
+	putchar('\n');
+
+	return 0;
+}
+
