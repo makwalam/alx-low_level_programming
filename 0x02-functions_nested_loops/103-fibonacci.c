07a1a969c506c573582ebd01e816a29cbf48f75d
makwalam <makwala7@gmail.com>
2023-06-21 04:43:38 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point of the program
  *
- * Description: Finds and prints the sum of even-valued terms in the Fibonacci sequence
+ * Description: Finds and prints the sum of even-valued terms in the
+ *              Fibonacci sequence
  *
  * Return: Always 0 (Success)
  */
