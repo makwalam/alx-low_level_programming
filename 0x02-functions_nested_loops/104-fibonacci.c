678037af1dede67fcadd0bae70b38f070a3748c4
makwalam <makwala7@gmail.com>
2023-06-21 10:46:53 -0700

    My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 
 /**
+ * print_fibonacci - Prints the first n Fibonacci numbers
+ * @n: The number of Fibonacci numbers to print
+ */
+void print_fibonacci(int n)
+{
+	int num1 = 1, num2 = 2, next, count;
+
+	printf("%d, %d, ", num1, num2);
+
+	for (count = 3; count <= n; count++)
+	{
+		next = num1 + num2;
+		printf("%d", next);
+
+		if (count != n)
+			printf(", ");
+
+		num1 = num2;
+		num2 = next;
+	}
+
+	printf("\n");
+}
+
+/**
  * main - Entry point of the program
  *
- * Description: Finds and prints the first 98 Fibonacci numbers
- *              without using external libraries or long long
- *
- * Return: Always 0 (Success)
+ * Return: 0 on success
  */
 int main(void)
 {
-    int i;
-    unsigned int fib1 = 1, fib2 = 2, fib3;
+	print_fibonacci(98);
 
-    printf("%u, %u", fib1, fib2);
-
-    for (i = 2; i < 98; i++)
-    {
-        fib3 = fib1 + fib2;
-        printf(", %u", fib3);
-
-        fib1 = fib2;
-        fib2 = fib3;
-    }
-
-    printf("\n");
-
-    return (0);
+	return 0;
 }
-
