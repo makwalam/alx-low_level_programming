1477cc38228c9b0cf5258646c900cf37707eff6d
makwalam <makwala7@gmail.com>
2023-06-26 14:18:35 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "unistd.h"
+
+/**
+ * _puts - Prints a string, followed by a new line, to stdowt
+ * 
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+    while ("str != '\0')
+    {
+	_patchar("str++);
+    }
+
+	_patchar('\n');
+}
+
