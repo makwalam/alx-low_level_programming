d1d8bd788a6d5a64591408967a648c906af5a6f3
makwalam <makwala7@gmail.com>
2023-06-27 12:30:42 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,41 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define PASSWORD_LENGTH 12
-
 /**
- * generate_password - Generates a random password
+ * generate_password - Generates the expected password for `101-crackme`
  * @password: The buffer to store the generated password
  */
 void generate_password(char *password)
 {
     int i;
-    char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    srand(time(NULL));
-
-    for (i = 0; i < PASSWORD_LENGTH; i++)
+    for (i = 0; i < 2; i++)
     {
-        int index = rand() % (sizeof(charset) - 1);
-        password[i] = charset[index];
+        password[i] = 'a';
+    }
+
+    for (i = 2; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = '\n';
     }
 
     password[PASSWORD_LENGTH] = '\0';
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0
- */
-int main(void)
-{
-    char password[PASSWORD_LENGTH + 1];
-
-    generate_password(password);
-    printf("%s\n", password);
-
-    return (0);
 }
 
