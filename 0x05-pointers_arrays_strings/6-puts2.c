43ab1b80c9665df4cfabbce375f753f967b1d025
makwalam <makwala7@gmail.com>
2023-06-27 13:50:57 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,15 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string.
- * @str: The input string.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void puts2(char *str)
+int main(void)
 {
-	int i;
+    char str[] = "Lorem ipsum...";  // Modify the string declaration
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		_putchar(str[i]);
-	}
-
-	_putchar('\n');
+    puts2(str);
+    return (0);
 }
 
