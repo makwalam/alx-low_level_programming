3dfc06c594ad3be64355760ab2c8aa1f878d3f9c
makwalam <makwala7@gmail.com>
2023-06-27 13:43:20 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,15 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string, followed by a new line
- * @str: The string to be printed
+ * puts2 - Prints every other character of a string.
+ * @str: The input string.
  */
 void puts2(char *str)
 {
 	int i;
 
-	if (str == NULL)
-		return;
-
 	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		/* Print characters at even indices */
 		_putchar(str[i]);
 	}
 
