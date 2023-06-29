279f07858a8dbe60f3665d939c7a38107b49b30d
makwalam <makwala7@gmail.com>
2023-06-29 16:52:25 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,27 +2,26 @@
 
 char *leet(char *str)
 {
+    char *ptr = str;
+    char *leet_str = malloc(sizeof(char) * 1337);  // Assuming the maximum length of the resulting string is 1337
+
     int i, j;
-    char *leet_str = malloc(sizeof(char) * strlen(str) + 1);
-    char *leet_map[] = {
-        "4", "3", "0", "7", "1",
-        "4", "3", "0", "7", "1",
-        "4", "3", "0", "7", "1",
-        "4", "3", "0", "7", "1",
-        "4", "3", "0", "7", "1",
-        "4", "3", "0", "7", "1"
-    };
+    char leet_chars[] = {'4', '3', '0', '7', '1'};
+    char original_chars[] = {'a', 'e', 'o', 't', 'l'};
 
     for (i = 0; str[i] != '\0'; i++)
     {
-        leet_str[i] = str[i];
-        for (j = 0; j < 26; j++)
+        for (j = 0; j < 5; j++)
         {
-            if (str[i] == 'a' + j || str[i] == 'A' + j)
+            if (str[i] == original_chars[j] || str[i] == (original_chars[j] - 'a' + 'A'))
             {
-                leet_str[i] = *leet_map[j];
+                leet_str[i] = leet_chars[j];
                 break;
             }
+        }
+        if (j == 5)
+        {
+            leet_str[i] = str[i];
         }
     }
 
