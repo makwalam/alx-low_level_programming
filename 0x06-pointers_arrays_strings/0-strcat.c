44a023183915eafb7400a0734135c0d4bea05b06
makwalam <makwala7@gmail.com>
2023-06-29 14:44:05 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,15 +11,14 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = 0;
 	int i;
 
-	/* Find the length of the destination string */
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	/* Concatenate at most n bytes from src to dest */
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[dest_len++] = src[i];
 
-	dest[dest_len] = '\0'; /* Add null terminator */
+	dest[dest_len] = '\0';
 
-	return (dest);
+	return dest;
 }
+
