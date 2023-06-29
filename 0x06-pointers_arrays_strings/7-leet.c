5cdc208cdc4b35edb540b89a4142f225ddd45b45
makwalam <makwala7@gmail.com>
2023-06-29 16:49:33 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,43 +1,32 @@
 #include <stdlib.h>
 
-/**
- * leet - Encodes a string into 1337.
- * @str: The input string to be encoded.
- *
- * Return: A pointer to the encoded string.
- */
 char *leet(char *str)
 {
-	char *ptr = str;
-	char *leetStr = malloc(sizeof(char) * 1024); /* Assuming maximum string length of 1024 */
+    int i, j;
+    char *leet_str = malloc(sizeof(char) * strlen(str) + 1);
+    char *leet_map[] = {
+        "4", "3", "0", "7", "1",
+        "4", "3", "0", "7", "1",
+        "4", "3", "0", "7", "1",
+        "4", "3", "0", "7", "1",
+        "4", "3", "0", "7", "1",
+        "4", "3", "0", "7", "1"
+    };
 
-	if (leetStr == NULL)
-		return NULL; /* Error in memory allocation */
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        leet_str[i] = str[i];
+        for (j = 0; j < 26; j++)
+        {
+            if (str[i] == 'a' + j || str[i] == 'A' + j)
+            {
+                leet_str[i] = *leet_map[j];
+                break;
+            }
+        }
+    }
 
-	int i, j;
-	char leetChars[] = "4433007711";
-	char replaceChars[] = "aAeEoOtTlL";
-
-	i = 0;
-	while (*ptr != '\0')
-	{
-		j = 0;
-		while (replaceChars[j] != '\0')
-		{
-			if (*ptr == replaceChars[j])
-			{
-				leetStr[i] = leetChars[j];
-				break;
-			}
-			j++;
-		}
-		if (replaceChars[j] == '\0')
-			leetStr[i] = *ptr;
-		ptr++;
-		i++;
-	}
-
-	leetStr[i] = '\0';
-	return leetStr;
+    leet_str[i] = '\0';
+    return leet_str;
 }
 
