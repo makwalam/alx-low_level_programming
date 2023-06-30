679d35ca96e537875bf81b1bab18dbe27023443b
makwalam <makwala7@gmail.com>
2023-06-30 13:49:16 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,18 +1,26 @@
 #include <stdlib.h>
 
-char *rot13(char *str) {
-  char *ptr = str;
-  char c;
+/**
+ * rot13 - Encodes a string using ROT13 cipher
+ * @str: The string to be encoded
+ *
+ * Return: A pointer to the encoded string
+ */
+char *rot13(char *str)
+{
+	char *ptr = str;
+	char c;
 
-  while ((c = *ptr) != '\0') {
-    if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
-      *ptr += 13;
-    else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
-      *ptr -= 13;
+	while ((c = *ptr) != '\0')
+	{
+		if ((c >= 'a' && c <= 'm') || (c >= 'A' && c <= 'M'))
+			*ptr += 13;
+		else if ((c >= 'n' && c <= 'z') || (c >= 'N' && c <= 'Z'))
+			*ptr -= 13;
 
-    ptr++;
-  }
+		ptr++;
+	}
 
-  return str;
+	return str;
 }
 
