239736bbb1b0a2ac70323ea454df52a2e2c5182f
makwalam <makwala7@gmail.com>
2023-06-30 02:23:43 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,38 +1,45 @@
 #include <stdlib.h>
+#include "main.h"
 
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The input string to be encoded.
+ *
+ * Return: A pointer to the encoded string.
+ */
 char *leet(char *str)
 {
-    char *ptr = str;
-    char *leetStr = malloc(sizeof(char) * 100); // Assuming the encoded string won't exceed 100 characters
-    int i, j;
-    char leetLetters[] = "aAeEoOtTlL";
-    char leetChars[] = "4433007711";
+	char *ptr = str;
+	char *leetStr = malloc(sizeof(char) * 100); /* Assuming the encoded string won't exceed 100 characters */
+	int i, j;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetChars[] = "4433007711";
 
-    if (leetStr == NULL)
-        return NULL;
+	if (leetStr == NULL)
+		return NULL;
 
-    // Copy the original string while encoding characters
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; leetLetters[j] != '\0'; j++)
-        {
-            if (str[i] == leetLetters[j])
-            {
-                leetStr[i] = leetChars[j];
-                break;
-            }
-        }
-        if (leetLetters[j] == '\0')
-            leetStr[i] = str[i];
-    }
-    leetStr[i] = '\0';
+	/* Copy the original string while encoding characters */
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leetLetters[j] != '\0'; j++)
+		{
+			if (str[i] == leetLetters[j])
+			{
+				leetStr[i] = leetChars[j];
+				break;
+			}
+		}
+		if (leetLetters[j] == '\0')
+			leetStr[i] = str[i];
+	}
+	leetStr[i] = '\0';
 
-    // Copy the encoded string back to the original string
-    for (i = 0; leetStr[i] != '\0'; i++)
-        ptr[i] = leetStr[i];
-    ptr[i] = '\0';
+	/* Copy the encoded string back to the original string */
+	for (i = 0; leetStr[i] != '\0'; i++)
+		ptr[i] = leetStr[i];
+	ptr[i] = '\0';
 
-    free(leetStr);
-    return str;
+	free(leetStr);
+	return str;
 }
 
