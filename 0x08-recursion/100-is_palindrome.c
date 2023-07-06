14136a3d5fc564bc9ef1c1b560d1c5984717478e
makwalam <makwala7@gmail.com>
2023-07-06 14:26:06 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,10 +13,10 @@
 int is_palindrome_recursive(char *s, int start, int end)
 {
 	if (start >= end)
-		return 1; /* Base case: an empty string or a string with one character is a palindrome */
+		return (1); /* Base case: an empty string or a string with one character is a palindrome */
 
 	if (s[start] != s[end])
-		return 0; /* Not a palindrome */
+		return (0); /* Not a palindrome */
 
 	return is_palindrome_recursive(s, start + 1, end - 1); /* Recursively check the remaining characters */
 }
@@ -30,6 +30,6 @@ int is_palindrome_recursive(char *s, int start, int end)
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
-	return is_palindrome_recursive(s, 0, len - 1);
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
 
