0b6ce386d5be1158945a701198e34ea7179501dc
makwalam <makwala7@gmail.com>
2023-07-09 13:06:16 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,6 +2,10 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _atoi(char *s);
+int print_name(char *name);
+int print_number_of_arguments(int argc);
+int print_arguments(char *argv[]);
+int multiply_numbers(int num1, int num2);
+int add_numbers(int argc, char *argv[]);
 
-#endif	
+#endif /* MAIN_H */
