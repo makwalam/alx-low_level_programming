72d60c59909c42f71c317ed79b00cf14c744db9c
makwalam <makwala7@gmail.com>
2023-07-10 16:23:52 -0700

    updated

---
+++ 0x0B-malloc_free/1-main.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s;
+
+    s = _strdup("ALX SE");
+    if (s == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return (1);
+    }
+    printf("%s\n", s);
+    free(s);
+    return (0);
+}
+julien@ubuntu:~/0x0a. malloc, free$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 1-main.c 1-strdup.c -o s
+julien@ubuntu:~/0x0a. malloc, free$ ./s 
+ALX SE
+julien@ubuntu:~/0x0a. malloc, free$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x0B-malloc_free
+File: 1-strdup.c
