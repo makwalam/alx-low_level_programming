0eda7212a49edbcb472648f85c51aa508d43b465
makwalam <makwala7@gmail.com>
2023-07-10 14:48:10 -0700

    updated

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,7 +2,12 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _atoi(char *s);
+void print_program_name(char *name);
+void print_argument_count(int count);
+void print_arguments(char **argv, int argc);
+int multiply_numbers(int num1, int num2);
+int add_positive_numbers(int argc, char **argv);
+int get_minimum_coins(int cents);
 
-#endi
+#endif /* MAIN_H */
 
