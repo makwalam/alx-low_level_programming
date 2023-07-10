275aa4508e23a41e09f5c9e291430ab97f210640
makwalam <makwala7@gmail.com>
2023-07-10 14:34:22 -0700

    updated

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int print_name(char *name);
-int print_number_of_arguments(int argc);
-int print_arguments(char *argv[]);
-int multiply_numbers(int num1, int num2);
-int add_numbers(int argc, char *argv[]);
+int _atoi(char *s);
 
-#endif /* MAIN_H */
+#endi
+
